f9430ae595b6427605bc1ade708ed8cafec2ddcc
Keziah Odoi <oreeshorah@gmail.com>
2023-07-07 20:42:48 +0000

    Alternative faster solution for Max Sub Array

--- src/patterns/slidingWindow/MaxSumSubArrayK.java
+++ src/patterns/slidingWindow/MaxSumSubArrayK.java
@@ -14,40 +14,29 @@ public class MaxSumSubArrayK {
         }
         int windowStart = 0;
         int length = arr.length;
-        int[] result;
         int sum = 0;
-//        int start = 0, end = 0, newMax = 0;
+        int start = 0, end = 0, newMax = 0;
         int max = 0;
-        List<Integer> list = new ArrayList<>();
-        Map<Integer, ArrayList<Integer>> resultMap = new HashMap<>();
-
         for (int windowEnd = 0; windowEnd < length; windowEnd++) {
             sum += arr[windowEnd];
-            list.add(arr[windowEnd]);
             if (windowEnd >= k - 1) {
-//                if (sum > newMax) {
-//                    newMax = sum;
-//                    start = windowStart;
-//                    end = windowEnd;
-//                }
-                resultMap.put(sum, new ArrayList<>(list));
+                if (sum > newMax) {
+                    newMax = sum;
+                    start = windowStart;
+                    end = windowEnd;
+                }
                 sum -= arr[windowStart];
                 windowStart++;
-                list.remove(0);
             }
 
         }
-        for (Map.Entry<Integer, ArrayList<Integer>> map : resultMap.entrySet()) {
-            max = Math.max(map.getKey(), max);
-        }
-//        int []finalResult = Arrays.copyOfRange(arr, start, end + 1);
-        result = resultMap.get(max).stream().mapToInt(i -> i).toArray();
-        return result;
+
+        return Arrays.copyOfRange(arr, start, end + 1);
     }
 
     public static void main(String[] args) {
-        int[] result = findSubArrayWithMaxSum(new int[]{2, 1, 5, 1, 3, 2}, 3);
-//        int[] result = findSubArrayWithMaxSum(new int[]{2, 3, 4, 1, 5}, 2);
+//        int[] result = findSubArrayWithMaxSum(new int[]{2, 1, 5, 1, 3, 2}, 3);
+        int[] result = findSubArrayWithMaxSum(new int[]{2, 3, 4, 1, 5}, 2);
 
         System.out.println("result array " + Arrays.toString(result));
     }
